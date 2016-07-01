7d043ad9407f462c58c6f9e27e66c71e680cae0c
gigabit101 <thegigabit101@gmail.com>
2016-07-01 02:03:06 +0100

    fixed shift clicking

--- src/main/java/crystekteam/crystek/container/ContainerBase.java
+++ src/main/java/crystekteam/crystek/container/ContainerBase.java
@@ -114,129 +114,37 @@ public class ContainerBase extends Container
     /*
     * Handles shift clicking for all machines
     */
+
     @Override
-    public ItemStack transferStackInSlot(EntityPlayer player, int slotIndex)
+    public ItemStack transferStackInSlot(EntityPlayer p, int i)
     {
-        ItemStack originalStack = null;
-        Slot slot = (Slot) inventorySlots.get(slotIndex);
-        int numSlots = inventorySlots.size();
+        ItemStack itemstack = null;
+        Slot slot = this.inventorySlots.get(i);
         if (slot != null && slot.getHasStack())
         {
-            ItemStack stackInSlot = slot.getStack();
-            originalStack = stackInSlot.copy();
-            if (slotIndex >= numSlots - 9 * 4 && tryShiftItem(stackInSlot, numSlots))
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+            int playerInv = 30;
+            if (i < this.tile.getSizeInventory() + playerInv)
             {
-                // NOOP
-            } else if (slotIndex >= numSlots - 9 * 4 && slotIndex < numSlots - 9)
-            {
-                if (!shiftItemStack(stackInSlot, numSlots - 9, numSlots))
+                if (!this.mergeItemStack(itemstack1, this.tile.getSizeInventory() + playerInv, this.inventorySlots.size(), true))
                 {
                     return null;
                 }
-            } else if (slotIndex >= numSlots - 9 && slotIndex < numSlots)
-            {
-                if (!shiftItemStack(stackInSlot, numSlots - 9 * 4, numSlots - 9))
-                {
-                    return null;
-                }
-            } else if (!shiftItemStack(stackInSlot, numSlots - 9 * 4, numSlots))
+            }
+            else if (!this.mergeItemStack(itemstack1, 0, this.tile.getSizeInventory() + playerInv, false))
             {
                 return null;
             }
-            slot.onSlotChange(stackInSlot, originalStack);
-            if (stackInSlot.stackSize <= 0)
+            if (itemstack1.stackSize == 0)
             {
                 slot.putStack(null);
-            } else
+            }
+            else
             {
                 slot.onSlotChanged();
             }
-            if (stackInSlot.stackSize == originalStack.stackSize)
-            {
-                return null;
-            }
-            slot.onPickupFromSlot(player, stackInSlot);
         }
-        return originalStack;
-    }
-
-    protected boolean shiftItemStack(ItemStack stackToShift, int start, int end)
-    {
-        boolean changed = false;
-        if (stackToShift.isStackable())
-        {
-            for (int slotIndex = start; stackToShift.stackSize > 0 && slotIndex < end; slotIndex++)
-            {
-                Slot slot = (Slot) inventorySlots.get(slotIndex);
-                ItemStack stackInSlot = slot.getStack();
-                if (stackInSlot != null && canStacksMerge(stackInSlot, stackToShift))
-                {
-                    int resultingStackSize = stackInSlot.stackSize + stackToShift.stackSize;
-                    int max = Math.min(stackToShift.getMaxStackSize(), slot.getSlotStackLimit());
-                    if (resultingStackSize <= max)
-                    {
-                        stackToShift.stackSize = 0;
-                        stackInSlot.stackSize = resultingStackSize;
-                        slot.onSlotChanged();
-                        changed = true;
-                    } else if (stackInSlot.stackSize < max)
-                    {
-                        stackToShift.stackSize -= max - stackInSlot.stackSize;
-                        stackInSlot.stackSize = max;
-                        slot.onSlotChanged();
-                        changed = true;
-                    }
-                }
-            }
-        }
-        if (stackToShift.stackSize > 0)
-        {
-            for (int slotIndex = start; stackToShift.stackSize > 0 && slotIndex < end; slotIndex++)
-            {
-                Slot slot = (Slot) inventorySlots.get(slotIndex);
-                ItemStack stackInSlot = slot.getStack();
-                if (stackInSlot == null)
-                {
-                    int max = Math.min(stackToShift.getMaxStackSize(), slot.getSlotStackLimit());
-                    stackInSlot = stackToShift.copy();
-                    stackInSlot.stackSize = Math.min(stackToShift.stackSize, max);
-                    stackToShift.stackSize -= stackInSlot.stackSize;
-                    slot.putStack(stackInSlot);
-                    slot.onSlotChanged();
-                    changed = true;
-                }
-            }
-        }
-        return changed;
-    }
-
-    private boolean tryShiftItem(ItemStack stackToShift, int numSlots)
-    {
-        for (int machineIndex = 0; machineIndex < numSlots - 9 * 4; machineIndex++)
-        {
-            Slot slot = (Slot) inventorySlots.get(machineIndex);
-            if (!slot.isItemValid(stackToShift))
-                continue;
-            if (shiftItemStack(stackToShift, machineIndex, machineIndex + 1))
-                return true;
-        }
-        return false;
-    }
-
-    public static boolean canStacksMerge(ItemStack stack1, ItemStack stack2)
-    {
-        if (stack1 == null || stack2 == null)
-        {
-            return false;
-        }
-        if (!stack1.isItemEqual(stack2))
-        {
-            return false;
-        }
-        if (!ItemStack.areItemStackTagsEqual(stack1, stack2))
-        {
-            return false;
-        }
-        return true;
+        return itemstack;
     }
 }
