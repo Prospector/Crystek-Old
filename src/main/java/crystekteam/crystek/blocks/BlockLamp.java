6398c6eaec83f389a42b60313cd05a256320b51f
Prospector <thedoctorsoda@gmail.com>
2016-07-26 11:01:18 -0700

    stuff n things

---
+++ src/main/java/crystekteam/crystek/blocks/BlockLamp.java
@@ -0,0 +1,11 @@
+package crystekteam.crystek.blocks;
+
+import crystekteam.crystek.lib.ModInfo;
+
+public class BlockLamp extends BlockBase
+{
+	public BlockLamp()
+	{
+		setUnlocalizedName(ModInfo.MOD_ID.toLowerCase() + ".lamp");
+	}
+}
--- src/main/java/crystekteam/crystek/client/render/TileTinkerRender.java
+++ src/main/java/crystekteam/crystek/client/render/TileTinkerRender.java
@@ -5,6 +5,7 @@ import crystekteam.crystek.util.RenderUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
 /**
@@ -12,24 +13,31 @@ import net.minecraft.item.ItemStack;
  */
 public class TileTinkerRender extends TileEntitySpecialRenderer<TileTinkerTable>
 {
-    @Override
-    public void renderTileEntityAt(TileTinkerTable te, double x, double y, double z, float partialTicks, int destroyStage)
-    {
-        if(te != null)
-        {
-            if(te.getStackInSlot(1) != null)
-            {
-                GlStateManager.pushMatrix();
+	@Override public void renderTileEntityAt(TileTinkerTable te, double x, double y, double z, float partialTicks,
+			int destroyStage)
+	{
+		if (te != null)
+		{
+			if (te.getStackInSlot(1) != null)
+			{
+				GlStateManager.pushMatrix();
 
-                ItemStack stack = te.getStackInSlot(1);
+				ItemStack stack = te.getStackInSlot(1);
 
-                double spin = Minecraft.getSystemTime()/800D;
-                GlStateManager.translate(x + .5, y + 1.5, z + .5);
-                GlStateManager.rotate((float)(((spin*40D)%360)), 0, 1, 0);
+				double spin = Minecraft.getSystemTime() / 1000D;
 
-                RenderUtil.renderItemInWorld(stack);
-                GlStateManager.popMatrix();
-            }
-        }
-    }
+				if (stack.getItem() instanceof ItemBlock)
+				{
+					GlStateManager.translate(x + .5, y + .79, z + .5);
+				} else
+				{
+					GlStateManager.translate(x + .5, y + .85, z + .5);
+				}
+				GlStateManager.rotate((float) (((spin * 40D) % 360)), 0, 1, 0);
+
+				RenderUtil.renderItemInWorld(stack);
+				GlStateManager.popMatrix();
+			}
+		}
+	}
 }
