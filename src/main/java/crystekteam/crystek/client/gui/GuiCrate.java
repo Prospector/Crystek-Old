a40f52aab371b86acb2d484d646f4840513d997e
gigabit101 <thegigabit101@gmail.com>
2016-08-09 01:31:59 +0100

    added slots to crate

--- src/main/java/crystekteam/crystek/client/gui/GuiCrate.java
+++ src/main/java/crystekteam/crystek/client/gui/GuiCrate.java
@@ -4,18 +4,17 @@ import crystekteam.crystek.container.ContainerCrate;
 import crystekteam.crystek.tiles.TileCrate;
 import crystekteam.crystek.tiles.prefab.TileBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Slot;
 
 /**
  * Created by Gigabit101 on 24/07/2016.
  */
 public class GuiCrate extends GuiBase
 {
-//    private static final ResourceLocation textureloc = new ResourceLocation(ModInfo.MOD_NAME.toLowerCase() + ":" +"textures/gui/crate.png");
-
     public TileCrate table;
     public GuiCrate(EntityPlayer player, TileBase tile)
     {
-        super(player, tile, new ContainerCrate(tile, player), "crate");//, textureloc);
+        super(player, tile, new ContainerCrate(tile, player), "crate");
         this.table = (TileCrate) tile;
     }
 
@@ -26,5 +25,13 @@ public class GuiCrate extends GuiBase
     protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_)
     {
         super.drawGuiContainerBackgroundLayer(p_146976_1_, p_146976_2_, p_146976_3_);
+        int i;
+        for (i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 9; ++j)
+            {
+                builder.drawSlot(this, guiLeft + 7 + j * 18, guiTop + 16 + i * 18);
+            }
+        }
     }
 }
