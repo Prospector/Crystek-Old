c0bf21750a01b28b1d82958046610535d4cf7e91
gigabit101 <thegigabit101@gmail.com>
2016-08-09 01:32:21 +0100

    more cleaning

--- src/main/java/crystekteam/crystek/client/gui/GuiBase.java
+++ src/main/java/crystekteam/crystek/client/gui/GuiBase.java
@@ -39,7 +39,6 @@ public class GuiBase extends GuiContainer
     protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int mouseX, int mouseY)
     {
         builder.drawDefaultBackground(this, guiLeft, guiTop, xSize, ySize);
-        builder.drawString(this, I18n.translateToLocal("tile." + name + ".name"), guiLeft + 65, guiTop + 10);
         builder.drawPlayerSlots(this, guiLeft + xSize / 2, guiTop + 83, true);
     }
 
@@ -47,23 +46,6 @@ public class GuiBase extends GuiContainer
 	{
         builder.drawEnergyBar(this, 5, 5, 70, (int) container.power, (int) tile.getMaxCapacity(), mouseX - guiLeft, mouseY - guiTop, "Tesla");
 	}
-
-//
-//    @Deprecated
-//	public void drawBurn(TileBase tile, int x, int y)
-//	{
-//		int k = (this.width - this.xSize) / 2;
-//		int l = (this.height - this.ySize) / 2;
-//
-//		if (tile != null)
-//		{
-//			int j = this.container.burnTime;
-//			this.mc.getTextureManager().bindTexture(overlays);
-//
-//			if (j > 0)
-//				this.drawTexturedModalRect(k + x, l + y + 12 - j, 0, 62 - j, 14, j + 2);
-//		}
-//	}
 
 
 	public void drawTankOverlay(TileBase tile, int x, int y)
