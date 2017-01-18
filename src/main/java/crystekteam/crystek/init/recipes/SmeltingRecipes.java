58c59d8b45fb8a0b882fd5ddf2aa172bb0335797
ProfessorProspector <thedoctorsoda@gmail.com>
2017-01-17 17:16:23 -0800

    obsidian ingot recipe

--- src/main/java/crystekteam/crystek/init/recipes/SmeltingRecipes.java
+++ src/main/java/crystekteam/crystek/init/recipes/SmeltingRecipes.java
@@ -1,5 +1,8 @@
 package crystekteam.crystek.init.recipes;
 
+import crystekteam.crystek.init.CrystekItems;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import reborncore.common.recipes.RCRecipeMethods;
 
 /**
@@ -7,6 +10,14 @@ import reborncore.common.recipes.RCRecipeMethods;
  */
 public class SmeltingRecipes extends RCRecipeMethods {
 	public static void init() {
+		register(CrystekItems.MATERIALS.getStack("obsidian_dust"), CrystekItems.MATERIALS.getStack("obsidian_ingot"), 0.8F);
+	}
 
+	static void register(ItemStack output, ItemStack input, float xp) {
+		GameRegistry.addSmelting(output, input, xp);
+	}
+
+	static void register(ItemStack input, ItemStack output) {
+		GameRegistry.addSmelting(input, output, 0.3F);
 	}
 }
