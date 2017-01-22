d5ae2faf6afb4e41aac0d4f137875a89febd2a22
Prospector <thedoctorsoda@gmail.com>
2017-01-21 17:11:51 -0800

    grinder json

--- src/main/java/crystekteam/crystek/event/CrystekEventHandler.java
+++ src/main/java/crystekteam/crystek/event/CrystekEventHandler.java
@@ -2,6 +2,7 @@ package crystekteam.crystek.event;
 
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 /**
  * Created by Prospector
@@ -10,5 +11,10 @@ public class CrystekEventHandler {
 	@SubscribeEvent
 	public void worldEventLoad(WorldEvent.Load event) {
 		event.getWorld().addEventListener(new CrystekWorldEventListener(event.getWorld()));
+	}
+
+	@SubscribeEvent
+	public void onCraft(PlayerEvent.ItemCraftedEvent event){
+		
 	}
 }
